812e626ba598e54d06a9f2ae1e54730a862de75f
James Colannino <james@colannino.org>
2012-11-01 13:06:31 -0700

    began implementing parseCommand and the new command struct

--- src/command.c
+++ src/command.c
@@ -9,34 +9,46 @@
 #include "string.h"
 #include "action.h"
 
+/* All commands are parsed down to a required verb and an optional direct
+   object and preposition + indirect object */
+typedef struct command {
+   char *verb;
+   char *directObject;
+   char *preposition;
+   char *indirectObject;
+} Command;
+
 /* executes a user input command */
 void executeCommand();
+
+/* main parse function that breaks a sentence down into its command parts */
+static Command parseCommand(char *sentence);
 
 
 void executeCommand() {
 
-   char *command;
+   Command command;
 
-   command = readCommand();
+   command = parseCommand(readCommand());
 
-   if (0 == strncmp(command, "north", MAX_COMMAND_LENGTH)) {
+   if (0 == strncmp(command.verb, "north", MAX_COMMAND_LENGTH)) {
       move(NORTH);
    }
 
-   else if (0 == strncmp(command, "south", MAX_COMMAND_LENGTH)) {
+   else if (0 == strncmp(command.verb, "south", MAX_COMMAND_LENGTH)) {
       move(SOUTH);
    }
 
-   else if (0 == strncmp(command, "east", MAX_COMMAND_LENGTH)) {
+   else if (0 == strncmp(command.verb, "east", MAX_COMMAND_LENGTH)) {
       move(EAST);
    }
 
-   else if (0 == strncmp(command, "west", MAX_COMMAND_LENGTH)) {
+   else if (0 == strncmp(command.verb, "west", MAX_COMMAND_LENGTH)) {
       move(WEST);
    }
 
    // TODO: add support for synonyms and add exit as synonym
-   else if (0 == strncmp(command, "quit", MAX_COMMAND_LENGTH)) {
+   else if (0 == strncmp(command.verb, "quit", MAX_COMMAND_LENGTH)) {
       printf("Goodbye!\n");
       exit(EXIT_SUCCESS);
    }
@@ -49,5 +61,12 @@ void executeCommand() {
 }
 
 
+static Command parseCommand(char *sentence) {
 
+   Command command = {NULL, NULL, NULL, NULL};
+
+   // TODO
+   command.verb = sentence;
+   return command;
+}
 
