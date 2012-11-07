29b57648a01d72165e7aaeec0dbb9b95b8996a25
James Colannino <james@colannino.org>
2012-11-07 09:07:49 -0800

    nothing big, just moved the order of two defined structs

--- src/include/trogdor.h
+++ src/include/trogdor.h
@@ -9,6 +9,14 @@
 /* out of memory errors happen often enough to write a macro */
 #define PRINT_OUT_OF_MEMORY_ERROR {fprintf(stderr, "out of memory"); exit(EXIT_FAILURE);}
 
+
+/* Synonyms will be stored as keys in a hash table */
+typedef struct object {
+   dstring_t name;         /* name of the object */
+   dstring_t description;  /* the user reads this when seen the first time */
+   int seen;               /* whether or not the object has been seen */
+} Object;
+
 /* Represents a single room. Each direction is a pointer to whatever room is
    located in that direction.  If you can't go in that direction, it should be
    set to null.
@@ -33,13 +41,6 @@ typedef struct room {
       quickly operate on all objects when we enter a room. */
    GArray *objectList;
 } Room;
-
-/* Synonyms will be keys in the objects hash table */
-typedef struct object {
-   dstring_t name;         /* name of the object */
-   dstring_t description;  /* the user reads this when seen the first time */
-   int seen;               /* whether or not the object has been seen */
-} Object;
 
 
 #endif
