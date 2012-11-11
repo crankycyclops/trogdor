b1ff4b8f7826c55edd5c948da6bb657c39988aba
James Colannino <james@colannino.org>
2012-11-10 17:26:06 -0800

    just reorganized some stuff

--- src/actions.c
+++ src/actions.c
@@ -10,6 +10,9 @@
 #include "include/shell.h"
 
 
+/* psuedo action that frees allocated memory and quits the game */
+int quitGame(Command command);
+
 /* moves the user in the specified direction */
 int move(Command command);
 
@@ -22,9 +25,6 @@ static void takeObject(Object *object);
 /* disambiguates in the case where a name refers to more than one object */
 static Object *clarifyObject(GList *objects, int objectCount);
 
-/* psuedo action that frees allocated memory and quits the game */
-int quitGame(Command command);
-
 /******************************************************************************/
 
 int quitGame(Command command) {
@@ -33,6 +33,102 @@ int quitGame(Command command) {
    printf("Goodbye!\n");
    exit(EXIT_SUCCESS);
 }
+
+/******************************************************************************/
+
+int move(Command command) {
+
+   dstring_t direction;
+
+   /* user entered navigation command in the verb + direction format */
+   if (
+      0 != strcmp("north", dstrview(command.verb)) &&
+      0 != strcmp("south", dstrview(command.verb)) &&
+      0 != strcmp("east",  dstrview(command.verb)) &&
+      0 != strcmp("west",  dstrview(command.verb))
+   ) {
+
+      if (NULL != command.directObject) {
+         direction = command.directObject;
+      }
+
+      else if (NULL != command.indirectObject) {
+         direction = command.indirectObject;
+      }
+
+      else {
+         printf("Where would you like to go?\n");
+         return 1;
+      }
+   }
+
+   /* command was given as just a simple direction (e.g. "north") */
+   else {
+      direction = command.verb;
+   }
+
+   if (0 == strcmp("north", dstrview(direction))) {
+
+         if (NULL != location->north) {
+            location = location->north;
+            displayRoom(location);
+         }
+
+         else {
+            printf("You can't go that way!\n");
+         }
+
+         return 1;
+   }
+
+   else if (0 == strcmp("south", dstrview(direction))) {
+
+         if (NULL != location->south) {
+            location = location->south;
+            displayRoom(location);
+         }
+
+         else {
+            printf("You can't go that way!\n");
+         }
+
+         return 1;
+   }
+
+   else if (0 == strcmp("east", dstrview(direction))) {
+
+         if (NULL != location->east) {
+            location = location->east;
+            displayRoom(location);
+         }
+
+         else {
+            printf("You can't go that way!\n");
+         }
+
+         return 1;
+   }
+
+   else if (0 == strcmp("west", dstrview(direction))) {
+
+         if (NULL != location->west) {
+            location = location->west;
+            displayRoom(location);
+         }
+
+         else {
+            printf("You can't go that way!\n");
+         }
+
+         return 1;
+   }
+
+   // syntax error: no such direction
+   else {
+      return 0;
+   }
+}
+
 
 /******************************************************************************/
 
@@ -144,100 +240,5 @@ static Object *clarifyObject(GList *objects, int objectCount) {
    }
 
    return object;
-}
-
-/******************************************************************************/
-
-int move(Command command) {
-
-   dstring_t direction;
-
-   /* user entered navigation command in the verb + direction format */
-   if (
-      0 != strcmp("north", dstrview(command.verb)) &&
-      0 != strcmp("south", dstrview(command.verb)) &&
-      0 != strcmp("east",  dstrview(command.verb)) &&
-      0 != strcmp("west",  dstrview(command.verb))
-   ) {
-
-      if (NULL != command.directObject) {
-         direction = command.directObject;
-      }
-
-      else if (NULL != command.indirectObject) {
-         direction = command.indirectObject;
-      }
-
-      else {
-         printf("Where would you like to go?\n");
-         return 1;
-      }
-   }
-
-   /* command was given as just a simple direction (e.g. "north") */
-   else {
-      direction = command.verb;
-   }
-
-   if (0 == strcmp("north", dstrview(direction))) {
-
-         if (NULL != location->north) {
-            location = location->north;
-            displayRoom(location);
-         }
-
-         else {
-            printf("You can't go that way!\n");
-         }
-
-         return 1;
-   }
-
-   else if (0 == strcmp("south", dstrview(direction))) {
-
-         if (NULL != location->south) {
-            location = location->south;
-            displayRoom(location);
-         }
-
-         else {
-            printf("You can't go that way!\n");
-         }
-
-         return 1;
-   }
-
-   else if (0 == strcmp("east", dstrview(direction))) {
-
-         if (NULL != location->east) {
-            location = location->east;
-            displayRoom(location);
-         }
-
-         else {
-            printf("You can't go that way!\n");
-         }
-
-         return 1;
-   }
-
-   else if (0 == strcmp("west", dstrview(direction))) {
-
-         if (NULL != location->west) {
-            location = location->west;
-            displayRoom(location);
-         }
-
-         else {
-            printf("You can't go that way!\n");
-         }
-
-         return 1;
-   }
-
-   // syntax error: no such direction
-   else {
-      return 0;
-   }
 }
 
