693b19aacc604f0992b17069cb77ecc3eb6badf4
James Colannino <james@colannino.org>
2012-12-03 13:58:04 -0800

    #41: partially implemented timer work queue

--- src/core/include/timer.h
+++ src/core/include/timer.h
@@ -2,11 +2,42 @@
 #define TIMER_H
 
 
+typedef struct {
+
+   int id;              /* uniquely identifies jobs in the timer queue */
+   int initTime;        /* time when job was registered in the timer queue */
+   int interval;        /* timer ticks between each call to job() */
+   void (*job)(void *); /* function to execute on each interval */
+   void *argument;      /* this argument is passed to job() */
+
+   /* Number of times to execute the job before removing it from the queue.
+      This value is decremented on every tick of the clock, and when it
+      reaches 0, it's removed from the queue.  If set to -1, it will continue to
+      execute indefinitely until it is removed manually. */
+   int executions;
+
+} TimedJob;
+
+
+#ifndef TIMER_C
+
 /* starts the timer */
 extern void initTimer();
 
 /* get the current time (how many seconds the game has been running) */
 extern int getTime();
+
+/* Inserts a job into the timer queue.  Takes as input a pointer to the function
+   we want to execute, an argument to pass to that function, an interval and
+   the number of times we want the job to execute before removing it from the
+   queue. */
+extern unsigned int registerTimedJob(void (*job)(void *), void *argument,
+   int interval, int executions);
+
+/* removes job with the specified id from the timer queue */
+extern int deregisterTimedJob(unsigned int id);
+
+#endif
 
 
 #endif
--- src/core/timer.c
+++ src/core/timer.c
@@ -1,10 +1,13 @@
 
+#define TIMER_C
+
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "include/trogdor.h"
 #include "include/data.h"
 #include "include/state.h"
+#include "include/timer.h"
 
 
 /* starts the timer */
@@ -12,6 +15,17 @@ void initTimer();
 
 /* get the current time (how many seconds the game has been running) */
 int getTime();
+
+/* Inserts a job into the timer queue.  Takes as input a pointer to the function
+   we want to execute, an argument to pass to that function, an interval and
+   the number of times we want the job to execute before removing it from the
+   queue. */
+unsigned int registerTimedJob(void (*job)(void *), void *argument, int interval,
+   int executions);
+
+/* Removes job with the specified id from the timer queue.  Returns 1 if the
+   job was successfully removed and 0 if it was not. */
+int deregisterTimedJob(unsigned int id);
 
 /* calls tick() on each interval */
 static void *timer(void *threadId);
@@ -21,6 +35,12 @@ static void tick();
 
 /* current tick (continually incremented by the ticker) */
 static int gameTime = 0;
+
+/* queue of jobs to execute on each tick of the clock */
+static GList *workQueue = NULL;
+
+/* keeps track of the last assigned job id */
+static unsigned int lastJobID = 0;
 
 /******************************************************************************/
 
@@ -43,6 +63,54 @@ void initTimer() {
 
 /******************************************************************************/
 
+unsigned int registerTimedJob(void (*job)(void *), void *argument, int interval,
+int executions) {
+
+   TimedJob *newjob;
+
+   newjob = malloc(sizeof(TimedJob));
+   if (NULL == newjob) {
+      PRINT_OUT_OF_MEMORY_ERROR;
+   }
+
+   lastJobID++;
+
+   newjob->id = lastJobID;
+   newjob->initTime = gameTime;
+   newjob->interval = interval;
+   newjob->executions = executions;
+   newjob->job = job;
+   newjob->argument = argument;
+
+   workQueue = g_list_append(workQueue, newjob);
+
+   return newjob->id;
+}
+
+/******************************************************************************/
+
+int deregisterTimedJob(unsigned int id) {
+
+   GList *curJob = workQueue;
+
+   while (curJob != NULL) {
+
+      if (id == ((TimedJob *)curJob->data)->id) {
+         TimedJob *job = (TimedJob *)curJob->data;
+         free(job);
+         workQueue = g_list_delete_link(workQueue, curJob);
+         return 1;
+      }
+
+      curJob = curJob->next;
+   }
+
+   /* job was not found */
+   return 0;
+}
+
+/******************************************************************************/
+
 static void *timer(void *threadId) {
 
    while (1) {
@@ -60,7 +128,7 @@ static void tick() {
 
    // NOTE: only do locking if payload actually does anything...
    pthread_mutex_lock(&resourceMutex);
-   // TODO: tick payload
+   // TODO: execute work queue jobs
    pthread_mutex_unlock(&resourceMutex);
 
    return;
