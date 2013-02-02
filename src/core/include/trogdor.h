d6d00e1eae0ee21b8d29e3ecdc49a779890d8a3e
James Colannino <james@colannino.org>
2013-02-02 13:00:03 -0800

    added Entity container type

--- src/core/include/trogdor.h
+++ src/core/include/trogdor.h
@@ -10,6 +10,11 @@
 /* used to identify entities referenced by a void pointer */
 enum EntityType {entity_room, entity_player, entity_creature, entity_object};
 
+typedef struct {
+   void *entity;
+   enum EntityType type;
+} Entity;
+
 /* out of memory errors happen often enough to write a macro */
 #define PRINT_OUT_OF_MEMORY_ERROR {g_outputError("out of memory"); exit(EXIT_FAILURE);}
 
